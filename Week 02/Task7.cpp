03066b610982c78134d4f7398c2c299ca30e0b29
Mustafa-Noor <mus.noor12@gmail.com>
2023-09-16 19:08:01 +0500

    Homework

---
+++ Week 02/Task7.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+system ("color E0");
+cout <<"      .::---::..      " << endl;
+cout <<"   .-------------.    " << endl;
+cout <<" .-----------------.  " << endl;
+cout <<" ----------------:.   " << endl;
+cout <<":-----------::.       " << endl;
+cout <<"-----------:.         " << endl;
+cout <<":-------------:..     " << endl;
+cout <<" :----------------:.  " << endl;
+cout <<"  .---------------:   " << endl;
+cout <<"    .:---------:.     " << endl;
+}       
